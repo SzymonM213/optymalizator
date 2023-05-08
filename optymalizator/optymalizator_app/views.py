59d1020db14ac6a943db08e21b3595cbb747ffeb
Rentib <sbitner420@tutanota.com>
2023-05-08 20:41:03 +0200

    update todos

--- optymalizator/optymalizator_app/views.py
+++ optymalizator/optymalizator_app/views.py
@@ -2,12 +2,10 @@ from django.shortcuts import render
 
 from .wyszukiwarka import read
 
-
-# TODO: create database model (Kuba)
+# TODO: poprawić modele (Kuba)
 
 def home(request):
     return render(request, 'home/home.html')
-
 
 def search(request):
     if (request.method == 'POST'):
@@ -20,12 +18,12 @@ def search(request):
         context = {
             'json_list': json_list
         }
-        return render(request, 'home/search.html', context)
-    return home(request) # TODO: return search results (Mikołaj)
+        return render(request, 'home/search.html', context)  # TODO: ui do wyników wyszukiwania (Mikołaj)
+    return home(request)
 
 def optimize(request):
     if request.method == 'POST':
         selected = request.POST['selected']
         print(selected)
-        # TODO: parse selected into json (Szymon)
-    return render(request, 'optimize/optimize.html') # TODO: return optimize results - ui zrobione, ale trzeba jeszcze sparsować kontekst do htmla (Staszek)
+        # TODO: zwrócenie w request listy odpowiedników (Szymon)
+    return render(request, 'optimize/optimize.html')
