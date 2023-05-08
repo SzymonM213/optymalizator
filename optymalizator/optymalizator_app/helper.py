613e8e88442a105413fd1698823889b11c0d9f6e
Antoni Grodowski <53835371+AnthonyGrod@users.noreply.github.com>
2023-05-08 20:03:51 +0200

    Dodałem wyszukiwarkę która działa z DB. Zostało do zrobienia sortowanie

---
+++ optymalizator/optymalizator_app/helper.py
@@ -0,0 +1,109 @@
+import re
+
+# File with abbreviations that are acceptable within the search engine and the shortcut name used in the database.
+# Key: shortcut name used in the database, Value
+
+abbv_map = {
+    "tabl.": ["tabletki", "tabletka", "tabletek", "tab.", "tabl."],
+    "kaps.": ["kapsułki", "kaps.", "kapsułka", "kapsułek", "kapsulki, kapsulka, kapsulek"],
+}
+
+unit_list = ['mg', 'g']
+
+szt = ["szt."]
+
+
+# Converts occurences of abbreviations to the shortcut name used in the database.
+def convert_to_db_shorcut(text):
+    for key, value in abbv_map.items():
+        for v in value:
+            text = text.replace(v, key)
+    return text
+
+
+def one_space_between_number_and_word(text):
+    regex = r'(\d+\+*)(\s*)([a-zA-Z]+)'
+    match = re.search(regex, text)
+    if match:
+        text = re.sub(regex, match.group(1) + " " + match.group(3), text, count=1)
+    return text
+
+
+# Checks if input consists a valid name or substance. If it does, it cuts the sequence from the input and list [input, name/substance] is returned.
+def read_and_cut(split_input, all_phrases):
+    for i in range(1, len(split_input) + 1):                                    # all available sequence sizes
+        for j in range(0, len(split_input) - i + 1):                            # all available starting points
+            test_phrase = " ".join(split_input[j:j+i])
+            test_phrase = test_phrase.lower()
+            if (test_phrase in all_phrases):
+                # cut test phrase from split_input
+                split_input = split_input[:j] + split_input[j+i:]
+                return [split_input, test_phrase]
+    return [split_input, ""]
+
+
+# Checks if input consists a valid ean. If it does, it cuts the sequence from the input and list [input, ean] is returned.
+def read_and_cut_ean(split_input):
+    regex = r'^\d{8,14}$'
+    for i in range(len(split_input)):
+        match = re.search(regex, split_input[i])
+        if (match):
+            split_input = split_input[:i] + split_input[i+1:]
+            return [split_input, match.group(1)]
+    return [split_input, ""]
+
+
+# Returns first occurence of the first number and abbreviations in a string.
+# For example for string "50 tabl. 2 blistry po 25 tabl." it returns 50 tabl. abbv: kaps. tabl.
+def return_first_number_and_abbv(text):
+    regex = r'(\d+\s?\s?)(?=\s*(?:' + '|'.join(abbv_map.keys()) + '))(\s*(?:' + '|'.join(abbv_map.keys()) + '))'
+    match = re.search(regex, text)
+    if match:
+        text = re.sub(regex, '', text, count=1)
+        return [text, match.group(1) + match.group(2)]
+    return [text, ""]
+
+
+# Returns first occurence of the first number and units in a string. Units are: g, mg.
+def return_and_cut_number_and_unit(text):
+    regex = r'(\d+(?:[\.,]\d+)?(?:\+\d+(?:[\.,]\d+)?)*)(?:\s*)((' + '|'.join(unit_list) + r')\b)'
+    match = re.search(regex, text)
+    if match:
+        text = re.sub(regex, '', text, count=1)
+        return [text.strip(), match.group(1) + match.group(2)] # replace comma with dot for decimal
+    return [text, ""]
+
+
+# Returns first occurence of the first number and szt. in a string.
+def return_and_cut_number_and_szt(text):
+    regex = r'(\d+(?:\.\d+)?)\s*(szt\.?)'
+    match = re.search(regex, text)
+    if match:
+        text = re.sub(regex, '', text, count=1)
+        return [text, match.group(1) + match.group(2)]
+    return [text, ""]
+
+
+def return_list(data):
+    new_data = {}
+    for key, values in data.items():
+        if key == 'nazwa_postac_dawka':
+            new_keys = ['nazwa', 'postac', 'dawka']
+            for value in values:
+                value_parts = value.split(', ')
+                for i in range(len(new_keys)):
+                    new_key = new_keys[i]
+                    new_value = value_parts[i]
+                    if new_key in new_data:
+                        new_data[new_key].append(new_value)
+                    else:
+                        new_data.update({new_key: [new_value]})
+                    
+        elif key == 'ean':
+            new_data[key] = ['0' + str(value) for value in values]
+        else:
+            new_data[key] = values
+        
+    rows = [dict(zip(new_data.keys(), values)) for values in zip(*new_data.values())]
+
+    return rows
---
+++ optymalizator/optymalizator_app/wyszukiwarka.py
@@ -0,0 +1,114 @@
+import pandas as pd
+import re
+import json
+
+# from .helper import *
+# from .data_import import *
+from optymalizator_app.helper import *
+from optymalizator_app.data_import import *
+
+
+def write(filter_res):
+    if filter_res == None:
+        return None
+    df_res = df_xlsx.copy() # deep copy
+
+    for key, value in filter_res.items():
+        df_res = df_res[df_xlsx[key].str.lower().str.contains(value)]
+
+    df_res = df_res.iloc[:, 0:8]
+    json_str = df_res.to_json(orient='table', index=False)
+    json_dict = json.loads(json_str)
+
+    fields = [f['name'] for f in json_dict['schema']['fields']]
+    data = [list(d.values()) for d in json_dict['data']]
+    output_dict = {field: [d[i] for d in data] for i, field in enumerate(fields)}
+    new_dict = {k: v for k, v in output_dict.items()}
+    # print(new_dict)
+    res_list = return_list(new_dict)
+    # print("RESULT:")
+    # print(res_list)
+    return res_list
+
+
+def read(input):
+    filter_res = {}
+    input = convert_to_db_shorcut(input)
+    # print(input)
+    split_input = input.split(" ")
+    # print(split_input)
+
+    # Extracting and cutting ean if one exists.
+    ean_res = read_and_cut_ean(split_input)
+    split_input = ean_res[0]
+    input = " ".join(split_input)
+    ean = ean_res[1]
+    if (ean != ""):
+        return filter_res.update({"ean": ean})
+
+    # Extracting and cutting name or substance if they exist.
+    name_res = read_and_cut(split_input, all_names)
+    split_input = name_res[0]
+    input = " ".join(split_input)
+    name = name_res[1]
+    if (name != ""):
+        filter_res.update({"nazwa": name})
+        # print("nazwa: ")
+        # print(name)
+
+    substance_res = read_and_cut(split_input, all_substances)
+    split_input = substance_res[0]
+    input = " ".join(split_input)
+    substance = substance_res[1]
+    if (substance != ""):
+        filter_res.update({"substancja_czynna": substance})
+        # print("substancja_czynna: ")
+        # print(substance)
+
+    if (name == "" and substance == ""):
+        return None  # TODO return error
+    
+    # # Extracting and cutting dose if it exists.
+    dose_res = return_and_cut_number_and_unit(input)
+    dose = dose_res[1]
+    input = dose_res[0]
+    split_input = input.split(" ")
+    if (dose != ""):
+        dose = one_space_between_number_and_word(dose)
+        # filter_res.update({"dawka": dose})
+        # print("dawka: ")
+        # print(dose)
+
+    # Extracting content if it exists.
+    content_res = return_and_cut_number_and_szt(input)
+    content = content_res[1]
+    input = content_res[0]
+    split_input = input.split(" ")
+    if (content != ""):
+        content = one_space_between_number_and_word(content)
+        filter_res.update({"zawartosc_opakowania": content})
+        # print("zawartosc: ")
+        # print(content)
+
+    # Extracting form if it exists.
+    form_res = return_first_number_and_abbv(input)
+    form = form_res[1]
+    input = form_res[0]
+    if (form != ""):
+        form = one_space_between_number_and_word(form)
+        filter_res.update({"postac": form})
+        # print("postac: ")
+        # print(form)
+
+    print("input: ")
+    print(input)
+    if (input != "" and not input.isspace()):
+        print("ERROR")
+        return None
+    
+    # print(filter_res)
+    return write(filter_res)
+
+
+# input = "Desloratadinum 30 szt."
+# print(read(input))
